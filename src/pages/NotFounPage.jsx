60b4e82bcd3fefc9d5b215401e21edafe9f58aff
L-David-T <davidlez2mgk@gmail.com>
2021-10-31 09:24:52 -0600

    add not a found

--- src/pages/NotFounPage.jsx
+++ src/pages/NotFounPage.jsx
@@ -1,6 +1,6 @@
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className='page__notfound'>
       <h1>Page not found</h1>
       {/* <img src='' alt="not afound" /> */}
       <p>Sorry, this page does not exist.</p>
