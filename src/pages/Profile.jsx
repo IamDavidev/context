00ec2cc9551b24518f4c34046a8c49d9f799d14f
L-David-T <davidlez2mgk@gmail.com>
2021-11-01 14:11:40 -0600

    add user

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,15 @@
-import { resUser } from '../helpers/fetchUser';
-
+// import { resUser } from '../helpers/fetchUser';
+import { user } from '../helpers/fetchUser';
 const Profile = () => {
+  // console.log(user);
+  const { id, email, first_name, last_name, avatar } = user.data;
+  console.log(id);
   return (
     <div>
       <h1>this is Profile</h1>
       {/* <button onClick={resUser}  >handel user </button> */}
-      <p>{id}</p>
-      <strong>{res.id}</strong>
+    <p>{id}</p>
+      <strong>{email}</strong>
     </div>
   );
 };
