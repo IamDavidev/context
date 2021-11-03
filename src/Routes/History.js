700d300b55b081a31887b0171dfd17e202626114
L-David-T <davidlez2mgk@gmail.com>
2021-11-03 10:58:47 -0600

    add css in profile app

---
+++ src/Routes/History.js
@@ -0,0 +1,5 @@
+import { createBrowserHistory } from 'history';
+import { useHistory } from 'react-router-dom';
+export const $HistoryBrow = createBrowserHistory();
+
+export const History = useHistory();
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,42 @@
 import { useContext } from 'react';
 import UserContext from '../contexts/UserContext';
+// import { History } from '../Routes/History';
 const Profile = () => {
   const { user, setUser } = useContext(UserContext);
+
   return (
-    <div>
-      <h1>this is Profile</h1>
-    {
-      user ? (
-        <div>
-          <h2>{user.name}</h2>
-          <h2>{user.email}</h2>
-          <button onClick={() => setUser(null)}>Logout</button>
+    <div className='profile'>
+      {user ? (
+        <div className="profile__container">
+          <div className="profile__container__image">
+            <img
+              src={user.data.avatar}
+              className="profile__img"
+              alt={user.data.first_name}
+            />
+          </div>
+          <div className="profile__info">
+          <p>
+            Name : <strong>{user.data.first_name} </strong>{' '}
+          </p>
+          <p>
+          last Name : <strong>{user.data.last_name} </strong>{' '}
+          </p>
+          <p>
+            Email : <strong>{user.data.email} </strong>{' '}
+          </p>
+          <p>
+            description : <strong>{user.support.text} </strong>{' '}
+          </p>
+          <a href={user.support.url} target="_blank">LINK</a>
+          {/* <button onClick={handleOut}>Logout</button> */}
+          </div>
         </div>
       ) : (
         <div>
           <h2>Please Login</h2>
         </div>
-      )
-    }
+      )}
     </div>
   );
 };
